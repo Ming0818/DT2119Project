902cfa15d3482ea3b5e5d4b7b80b291d90565c86
Ubuntu <ubuntu@ip-172-31-10-238.eu-west-1.compute.internal>
2018-05-16 12:22:48 +0000

    cnn changes

--- cnn.py
+++ cnn.py
@@ -2,7 +2,7 @@ from network import Network
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from keras import Sequential
 import numpy as np
-from keras.layers import Conv2D, Flatten, Dense
+from keras.layers import Conv2D, Dropout, Flatten, Dense
 
 
 class CNN(Network):
@@ -14,17 +14,18 @@ class CNN(Network):
         self.x_val = np.expand_dims(self.x_val, 3)
         self.x_test = np.expand_dims(self.x_test, 3)
         model = Sequential()
-
         model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1),
                          activation='relu',
                          input_shape=(self.x_val.shape[1:])))
+        model.add(Dropout(0.3))
+        model.add(Conv2D(32, kernel_size=(3,3), strides=(1,1), activation='relu'))
         model.add(Flatten())
         model.add(Dense(128, activation='relu'))
         model.add(Dense(self.y_val.shape[1], activation='softmax'))
 
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-        history_callback = model.fit(self.x_val, self.y_val,
+        history_callback = model.fit(self.x_train, self.y_train,
                                      validation_data=(self.x_val, self.y_val),
                                      epochs=self.epochs,
                                      batch_size=2048, callbacks=[es, lr_reduce])
